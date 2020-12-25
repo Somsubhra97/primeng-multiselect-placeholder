1c8e9d6d21674efb46e503ce17bfae0c69306ac5
Somsubhra97 <somsubhrablasterjaxxmitra@gmail.com>
2020-12-25 12:09:14 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FormsModule } from "@angular/forms";
 import { DropdownModule } from "primeng/dropdown";
 import { MultiSelectModule } from "primeng/multiselect";
+import { ButtonModule } from "primeng/button";
 
 import { AppComponent } from "./app.component";
 
@@ -13,10 +14,10 @@ import { AppComponent } from "./app.component";
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-
     // PrimeNG
-    DropdownModule,    
-    MultiSelectModule
+    DropdownModule,
+    MultiSelectModule,
+    ButtonModule
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
